f0ce25a332339ce535a1e7a3701615affbb9f796
sehoon0302 <saehun66@gmail.com>
2023-10-28 16:37:58 +0900

    코틀린 조건문 - if문은 자바와 완전히 동일함 - when 문법 체험

--- app/src/main/java/kr/ac/wku/kotlinbasic/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/kotlinbasic/MainActivity.kt
@@ -45,13 +45,53 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this, myName, Toast.LENGTH_SHORT).show() //훈세유 문구 토스트
 
-            //어머니 이름 : 변경불가
+            //어머니 이름 : 변경불가 => 변수 생성과 동시에 저장
 
             val motherName : String
             motherName = "어머니"
 
             //어머니 이름 변경 시도
             //motherName = "어머니2" //syntax 에러 발생! => 상수로 선언한 공간의 값을 변경 시도
+
+            //내 출생년도 var에 저장. (변경가능) => 변수 생성과 동시에 저장
+            var myBirthYear = 1999  //자료형이 자동으로 Int로 추론되어 결정됨. (자료형 고정됨. 다른 종류의 값 대입 불가)
+            
+            //코틀린에서는 모든 변수가 전부 .이나  메소드 등 활용 가능(전부 참조형)
+            //myBirthYear.compareTo() => int도 참조형, 추가 기능 활용 가능
+
+        }
+        
+        binding.btnCondition.setOnClickListener { 
+            
+            //사용자의 나이를 val에 저장
+            val userAge = 25
+            
+            //20살 이상? 성인 문구 토스트로
+//            if (userAge >= 20){
+//                Toast.makeText(this, "성인입니다.", Toast.LENGTH_SHORT).show()
+//            }
+//            else if(userAge>=17){
+//                Toast.makeText(this, "고등학생입니다.", Toast.LENGTH_SHORT).show()
+//            }
+//            else{
+//                Toast.makeText(this, "중학생 또는 그 이하 입니다.", Toast.LENGTH_SHORT).show()
+//            }
+
+            //when 활용 예시
+            when(userAge){
+                25 -> {
+                    //userAge에 25가 들어있을때 실행할 코드
+                    Toast.makeText(this, "25살 입니다.", Toast.LENGTH_SHORT).show()
+                }
+                27, 28, 29 -> {
+                    //userAge의 값이 27,28,29 셋중 하나라면 실행
+                    Toast.makeText(this, "20대 후반입니다.", Toast.LENGTH_SHORT).show()
+                }
+                in 30..39 -> {
+                    //userAge의 값이 30이상 39이야인 경우
+                    Toast.makeText(this, "30대입니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
